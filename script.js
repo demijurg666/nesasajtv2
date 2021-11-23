d4443685a47750bdd98a238ad87475284a3a1d06
darkoSajtovi <44579515+demijurg666@users.noreply.github.com>
2021-11-23 23:48:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+function srpski(){
+    let izdavastvoPrevedi, kursPrevedi, mlinPrevedi, pricePrevedi, trgovinaPrevedi,pecanjePrevedi, pesmePrevedi, kontaktPrevedi
+
+    izdavastvoPrevedi = document.getElementById('izdavastvoPrevedi')
+    kursPrevedi = document.getElementById('kursPrevedi')
+    mlinPrevedi = document.getElementById('mlinPrevedi')
+    pricePrevedi = document.getElementById('pricePrevedi')
+    trgovinaPrevedi = document.getElementById('trgovinaPrevedi')
+    pecanjePrevedi = document.getElementById('pecanjePrevedi')
+    pesmePrevedi = document.getElementById('pesmePrevedi')
+    kontaktPrevedi = document.getElementById('kontaktPrevedi')
+
+
+    izdavastvoPrevedi.innerHTML = 'Izdavastvo';
+    kursPrevedi.innerHTML = 'Kurs';
+    mlinPrevedi.innerHTML = 'Mlin';
+    pricePrevedi.innerHTML = 'Price sa terena';
+    trgovinaPrevedi.innerHTML = 'Trgovina';
+    pecanjePrevedi.innerHTML = 'Pecanje';
+    pesmePrevedi.innerHTML = 'Pesme';
+    kontaktPrevedi.innerHTML = 'Kontakt';
+
+}
+
+function engleski(){
+    let izdavastvoPrevedi, kursPrevedi, mlinPrevedi, pricePrevedi, trgovinaPrevedi,pecanjePrevedi, pesmePrevedi, kontaktPrevedi
+
+    izdavastvoPrevedi = document.getElementById('izdavastvoPrevedi')
+    kursPrevedi = document.getElementById('kursPrevedi')
+    mlinPrevedi = document.getElementById('mlinPrevedi')
+    pricePrevedi = document.getElementById('pricePrevedi')
+    trgovinaPrevedi = document.getElementById('trgovinaPrevedi')
+    pecanjePrevedi = document.getElementById('pecanjePrevedi')
+    pesmePrevedi = document.getElementById('pesmePrevedi')
+    kontaktPrevedi = document.getElementById('kontaktPrevedi')
+
+
+    izdavastvoPrevedi.innerHTML = 'Publishing';
+    kursPrevedi.innerHTML = 'Course';
+    mlinPrevedi.innerHTML = 'Mill';
+    pricePrevedi.innerHTML = 'Stories from the field';
+    trgovinaPrevedi.innerHTML = 'Tradeing';
+    pecanjePrevedi.innerHTML = 'Fishing';
+    pesmePrevedi.innerHTML = 'Songs';
+    kontaktPrevedi.innerHTML = 'Contacts';
+
+}
+
+function kineski(){
+    let izdavastvoPrevedi, kursPrevedi, mlinPrevedi, pricePrevedi, trgovinaPrevedi,pecanjePrevedi, pesmePrevedi, kontaktPrevedi
+
+    izdavastvoPrevedi = document.getElementById('izdavastvoPrevedi')
+    kursPrevedi = document.getElementById('kursPrevedi')
+    mlinPrevedi = document.getElementById('mlinPrevedi')
+    pricePrevedi = document.getElementById('pricePrevedi')
+    trgovinaPrevedi = document.getElementById('trgovinaPrevedi')
+    pecanjePrevedi = document.getElementById('pecanjePrevedi')
+    pesmePrevedi = document.getElementById('pesmePrevedi')
+    kontaktPrevedi = document.getElementById('kontaktPrevedi')
+
+
+    izdavastvoPrevedi.innerHTML = '出版';
+    kursPrevedi.innerHTML = '一门课程';
+    mlinPrevedi.innerHTML = '磨';
+    pricePrevedi.innerHTML = '场地的故事';
+    trgovinaPrevedi.innerHTML = '交易';
+    pecanjePrevedi.innerHTML = '钓鱼';
+    pesmePrevedi.innerHTML = '歌曲';
+    kontaktPrevedi.innerHTML = '联系';
+
+}
